04fa20d26bf8062c6d75a296ccea299f9da85b04
own-idea <m_tfn@126.com>
2014-11-07 22:22:11 +0800

    add insert sort code

--- IntroductionToAlgorithms/src/main/java/introduction/to/algorithms/chapter1/InsertSort.java
+++ IntroductionToAlgorithms/src/main/java/introduction/to/algorithms/chapter1/InsertSort.java
@@ -1,7 +1,28 @@
 package introduction.to.algorithms.chapter1;
 
 public class InsertSort {
-	public static void sort(){
-		System.out.println("InsertSort");
+
+	public static void sort(int[] a) {
+		for (int j = 1; j < a.length; j++) {
+			int key = a[j];
+			for (int i = j - 1; i >= 0 && a[i] > key; i--) {
+				int temp = a[i];
+				a[i] = key;
+				a[i + 1] = temp;
+			}
+			// 第二种实现方式
+	/*		for (int i = j; i >0; i--) {
+				if(b[i-1]>b[i]){
+					int temp =b[i];
+					b[i]=b[i-1];
+					b[i-1]=temp;
+				}
+			}*/
+		}
 	}
+	
+	public static <T extends Comparable<T>> void sort(T[] array) {
+
+	}
+	
 }
